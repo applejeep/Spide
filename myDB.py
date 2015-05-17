2a6b9ead5e66b0edce9d299c7f6f399832096693
applejeep <294109237@qq.com>
2015-05-17 23:02:58 +0800

    update Spider douban.com

--- myDB.py
+++ myDB.py
@@ -8,7 +8,10 @@ class DB:
     def __init__(self):
         #self.loginUrl="https://www.douban.com/accounts/login"
         self.loginUrl="https://www.douban.com/accounts/login"
+        self.mail="http://www.douban.com/doumail/"
         self.cookies=cookielib.CookieJar()
+        self.user_agent='Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+        self.headers={'User-Agent':self.user_agent}
         self.postdata=urllib.urlencode({
             'form_email':'apple_jeep@163.com',
             'form_password':'' 
@@ -18,10 +21,16 @@ class DB:
     def getPage(self):
         request=urllib2.Request(
             url=self.loginUrl,
-            data=self.postdata)
+            data=self.postdata,
+            headers=self.headers)
         result=self.opener.open(request)
-        print result.read().decode('utf-8')
+        print 'login: ', result.read().decode('utf-8')
+
+        result=self.opener.open(self.mail)
+        print 'mail: ', result.read().decode('utf-8')
         #print 'response: ', response
+    
+        
 
 if __name__=="__main__":
     db=DB()
